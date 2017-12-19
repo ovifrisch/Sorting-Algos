6d1d313b9efd81ed814af5bd35f147e4e0c829c0
ovifrisch <33567249+ovifrisch@users.noreply.github.com>
2017-12-19 02:02:33 -0800

    Update mergesort.cpp

--- mergesort.cpp
+++ mergesort.cpp
@@ -2,13 +2,52 @@
 #include <vector>
 using namespace std;
 
+template<class T>
+void merge(vector<T>& list, int leftStart, int leftEnd, int rightStart, int rightEnd) {
+	int length = rightEnd - leftStart + 1;
+	vector<T> temp;
+	int left = leftStart;
+	int right = rightStart;
+	for (int i = 0; i < length; i++) {
+		if (left > leftEnd) {
+			temp.push_back(list[right++]);
+		}
+		else if (right > rightEnd) {
+			temp.push_back(list[left++]);
+		}
+		else if (list[left] <= list[right]) {
+			temp.push_back(list[left++]);
+		}
+		else {
+			temp.push_back(list[right++]);
+		}
+	}
 
-template <class Comparable>
-void mergesort(vector<Comparable>& unsorted, vector<Comparable>& sorted) {
-	
+	for (int i = 0; i < length; i++) {
+		list[leftStart++] = temp[i];
+	}
+}
+
+template <class T>
+void mergesort(vector<T>& list, int start, int end) {
+	if (start >= end) return;
+	int middle = (start + end) / 2;
+	mergesort(list, 0, middle);
+	mergesort(list, middle + 1, end);
+	merge(list, start, middle, middle + 1, end);
+}
+
+template <class T>
+void printList(vector<T> list) {
+	for (int i = 0; i < list.size(); i++) {
+		cout << list[i] << " ";
+	}
+	cout << endl;
 }
 
 int main(int argc, char** argv) {
-	vector<int> v = {5, 4, 3, 7, 9, 1, 2};
+	vector<int> list = {5, 4, 3, 7, 9, 1, 2};
+	vector<int> res;
+	mergesort(list, 0, list.size());
+	printList(list);
 }
-
