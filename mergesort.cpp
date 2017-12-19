90d696fdff5599b5c6d0ec789001958fa5067caf
ovifrisch <33567249+ovifrisch@users.noreply.github.com>
2017-12-19 00:53:40 -0800

    Update mergesort.cpp

--- mergesort.cpp
+++ mergesort.cpp
@@ -9,17 +9,6 @@ void mergesort(vector<Comparable>& unsorted, vector<Comparable>& sorted) {
 }
 
 int main(int argc, char** argv) {
-	vector<int> v;
-	v.push_back(7);
-	v.push_back(1);
-	v.push_back(3);
-	v.push_back(5);
-	v.push_back(8);
-	v.push_back(2);
-	v.push_back(4);
-	v.push_back(9);
-	v.push_back(6);
-
-	mergesort()
+	vector<int> v = {5, 4, 3, 7, 9, 1, 2};
 }
 
