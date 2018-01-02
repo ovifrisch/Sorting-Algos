80f2210837f1703fb73b64ee53eb08cc2f4a3026
Ovi Fritsch <oafritsch@ucdavis.edu>
2018-01-02 12:05:34 -0800

    first go program

--- BubbleSort/bubbleSort.go
+++ BubbleSort/bubbleSort.go
@@ -2,12 +2,29 @@ package main
 
 import "fmt"
 
-func bubbleSort() {
-	
+func print(list []int) {
+	for i := 0; i < len(list); i++ {
+		fmt.Print(list[i], " ")
+	}
+	fmt.Println()
+}
+
+func bubbleSort(list []int) {
+	for i := 0; i < len(list); i++ {
+		for j := 0; j < len(list) - 1; j++ {
+			if (list[j] > list[j + 1]) {
+				temp := list[j]
+				list[j] = list[j + 1]
+				list[j + 1] = temp
+			}
+		}
+	}
 }
 
 func main() {
-	fmt.Println("Helloworld")
+	list := []int{5, 4, 3, 2, 1}
+	bubbleSort(list)
+	print(list)
 }
 
 
